1570315b1474a6b8d2fe30bb36d799373bda911a
Bhakti Inamdar <bhaktiinamdar2212@gmail.com>
2024-05-09 15:02:17 -0400

    modified and added files

--- src/test/java/Pages/Loginpage.java
+++ src/test/java/Pages/Loginpage.java
@@ -5,5 +5,8 @@ public class Loginpage {
     public static void main(String a[])
     {
         System.out.println("Hello Login page");
+        String unm="Admin";
+        String pwd="Admin";
+        System.out.println("Logged in using given user credentials");
     }
 }
---
+++ src/test/java/Pages/Searchpage.java
@@ -0,0 +1,8 @@
+package Pages;
+
+public class Searchpage {
+    public static void main()
+    {
+        System.out.println("Hello Search page");
+    }
+}
